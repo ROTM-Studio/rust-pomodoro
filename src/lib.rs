7c192a433b1e00a579434dac56f0c49affbfc4bb
Faishal Risfandi <37602139+w0rm1995@users.noreply.github.com>
2021-12-24 21:24:53 +0800

    Implement method instead of function
    Signed-off-by: Faishal Risfandi <37602139+w0rm1995@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,56 @@ pub struct Config {
     pub session: u32,
 }
 
+impl Config {
+    pub fn new() -> Config {
+        Config::parse()
+    }
+
+    pub fn pomodoro(&self, is_done: Sender<bool>, is_break: Sender<bool>, is_start: Sender<u32>) {
+        let focus_in_second = self.focus_time * 60;
+        let break_in_second = self.break_time * 60;
+
+        let mut stdout = stdout();
+
+        for i in 0..self.session {
+            is_start.send(i + 1).unwrap();
+
+            print!("\x1b[41m");
+            print!("\x1b[37m");
+
+            for i in (0..focus_in_second).rev() {
+                let minute_remaining = i / 60;
+                let second_remaining = i % 60;
+
+                print!("\r{}:{} - Focus ", minute_remaining, second_remaining);
+                stdout.flush().unwrap();
+                thread::sleep(Duration::from_millis(100));
+            }
+
+            if i == self.session - 1 {
+                stdout.flush().unwrap();
+                print!("\r");
+                break;
+            }
+
+            is_break.send(true).unwrap();
+
+            print!("\x1b[42m");
+            print!("\x1b[30m");
+
+            for i in (0..break_in_second).rev() {
+                let minute_remaining = i / 60;
+                let second_remaining = i % 60;
+                print!("\r{}:{} - Break ", minute_remaining, second_remaining);
+                stdout.flush().unwrap();
+                thread::sleep(Duration::from_millis(100));
+            }
+        }
+
+        is_done.send(true).unwrap();
+    }
+}
+
 pub fn pomodoro(
     config: Config,
     is_done: Sender<bool>,
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use clap::Parser;
 use crossbeam::{channel::bounded, select};
 use rust_pomodoro::Config;
 use std::{process, thread};
 
 fn main() {
-    let config = Config::parse();
+    let config = Config::new();
 
     let (is_done_sender, is_done_receiver) = bounded(1);
     let (is_break_sender, is_break_receiver) = bounded(1);
     let (is_start_sender, is_start_receiver) = bounded(1);
 
-    thread::spawn(|| {
-        rust_pomodoro::pomodoro(config, is_done_sender, is_break_sender, is_start_sender)
+    thread::spawn(move || {
+        config.pomodoro(is_done_sender, is_break_sender, is_start_sender);
+        // rust_pomodoro::pomodoro(config, is_done_sender, is_break_sender, is_start_sender)
     });
 
     loop {
